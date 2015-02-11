e0161a964dd942aef195d571a6ac8af150698f16
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-02-11 15:32:44 +0900

    add blog create show listShow reload delete function

--- 01.blog/app/controllers/articles_controller.rb
+++ 01.blog/app/controllers/articles_controller.rb
@@ -13,10 +13,19 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
   def show
     @article = Article.find(params[:id])
@@ -26,5 +35,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
