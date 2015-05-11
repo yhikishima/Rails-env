9d8fcc1919abcababbcf05bd871c680b31922f8f
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-05-11 09:07:26 +0900

    tried to touch until the controller

--- 03.rails-appTest/app/controllers/users_controller.rb
+++ 03.rails-appTest/app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    if params[:username] == 'userName01'
+      @user[:name] = 'rails test'
+      @user[:username] = 'userName01'
+      @user[:location] = 'Tokyo, Japan'
+      @user[:about] = 'Nice to meet you.'
+    elsif params[:username] == 'yamada'
+      @user[:name] = 'yamada tarou'
+      @user[:username] = 'yamada'
+      @user[:location] = 'Tokyo, Japan'
+      @user[:about] = 'Nice to meet you.'
+    end
   end
 end
--- 03.rails-appTest/config/routes.rb
+++ 03.rails-appTest/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
 
-  get 'users/show'
+  get "users/show/:username" => "users#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
